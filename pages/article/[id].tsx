1c35e1f7a07815ee18ed301eda09441e6016dc49
AguirreEze <aguirre.eze95@gmail.com>
2022-04-04 16:36:10 -0300

    Feat Article Type

---
+++ pages/article/[id].tsx
@@ -0,0 +1,44 @@
+import { GetServerSideProps } from "next"
+import { getArticleData } from "services/article"
+import { ArticleType } from "types"
+import sanitizeHtml from "sanitize-html"
+import styles from "./styles.module.scss"
+
+interface Iprops {
+  data: ArticleType
+}
+
+export default function Article({ data }: Iprops) {
+  const date = new Date(data.published)
+  return (
+    <article className={styles.container}>
+      <h1>{data.title}</h1>
+      <div className={styles.tagsContainer}>
+        <h2>Tags :</h2>
+        <ul className={styles.tags}>
+          {data.tags.map((tag) => (
+            <li key={tag.id}>{tag.name}</li>
+          ))}
+        </ul>
+      </div>
+      <span className={styles.date}>Publicado {date.toDateString()}</span>
+      {data.author && <h2>Published by: {data.author}</h2>}
+
+      <div
+        dangerouslySetInnerHTML={{ __html: sanitizeHtml(data.content) }}
+      ></div>
+      <h2>Bibliografia</h2>
+      <div
+        dangerouslySetInnerHTML={{ __html: sanitizeHtml(data.bibliography) }}
+      ></div>
+    </article>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { id } = context.params!
+  if (!id) return { notFound: true }
+  const data = await getArticleData(id)
+
+  return { props: { data: data } }
+}
