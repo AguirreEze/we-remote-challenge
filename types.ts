bd506903e584d520285755f4dc9afd08001e128e
AguirreEze <aguirre.eze95@gmail.com>
2022-04-04 16:35:20 -0300

    feat article type and tag type

--- types.ts
+++ types.ts
@@ -1,18 +1,35 @@
 interface Categories {
   name: string
 }
-export interface ArticleType {
+export interface ArticlePreviewType {
   id: number
   title: string
   categories: Categories[]
   headline: string
+  /*eslint-disable */
   featured_media: {
     thumbnail: string
   }
+  /* eslint-enable */
 }
 
 export interface SeachResult {
-  data: ArticleType[]
+  data: ArticlePreviewType[]
   size: number
   pages: number
+}
+
+export interface Tags {
+  name: string
+  id: number
+}
+
+export interface ArticleType {
+  title: string
+  categories: Categories[]
+  published: string
+  content: string
+  tags: Tags[]
+  bibliography: string
+  author?: string
 }
